6cf7be57d6120602a972789786e686c083df81ac
Ryan Kes <alrayyes@gmail.com>
2019-08-05 20:05:23 +0200

    Converted tag string to array

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,9 +1,51 @@
+import { graphql } from 'gatsby'
 import React from 'react'
+import { HTMLContent } from '../components/content'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Template from './blog/template'
 
-const BlogPost: React.FunctionComponent = () => {
+interface IPost {
+  data: {
+    markdownRemark: {
+      html: string,
+      timeToRead: number,
+      frontmatter: {
+        title: string,
+        date: string,
+      },
+    },
+  }
+}
+
+const Post: React.FunctionComponent<IPost> = ({ data }) => {
+  const { html, frontmatter, timeToRead } = data.markdownRemark
+
   return (
-    <p>blog post</p>
+    <Layout>
+      <SEO title={frontmatter.title}/>
+      <Template
+        date={frontmatter.date}
+        title={frontmatter.title}
+        body={html}
+        contentComponent={HTMLContent}
+        timeToRead={timeToRead}
+      />
+    </Layout>
   )
 }
 
-export default BlogPost
+export default Post
+
+export const pageQuery = graphql`
+    query BlogPostTemplate ($id: String!) {
+        markdownRemark(id: { eq: $id }) {
+            html
+            timeToRead
+            frontmatter {
+                date
+                title
+            }
+        }
+    }
+`
